6d68efe3d1c382a351bc563993b9b6f95eddf5bf
Gokulgmiracle <gokulgandhi451@gmail.com>
2024-01-27 12:36:11 +0530

    first commit

---
+++ Text To Speak/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+   
+    let button = document.querySelector("button");
+    let text = document.querySelector("textarea");
+
+    button.addEventListener("click", () => {
+        let utterance = new SpeechSynthesisUtterance(text.value);
+        speechSynthesis.speak(utterance);
+    });
+});
